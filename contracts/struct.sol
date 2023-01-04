c2a1155e54697a04ca5b48d904aeeb16d8fcdab3
Bukasuji <chukwuebukaosuji@gmail.com>
2023-01-04 19:05:51 +0100

    updated getDetails function

--- contracts/struct.sol
+++ contracts/struct.sol
@@ -5,6 +5,7 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Deposit{
     
     mapping(address=>uint256) public balances;
+    mapping(string=>uint256) public kycDetails;
 
     struct UserDetails {
         string name;
@@ -27,14 +28,15 @@ contract Deposit{
  
     }
      
-     // function used to set details and pushed to the array
+     // function used to set details and pushed to the array and mapping
      function setUserDetails(string calldata name, uint256 age) public {
         detailArr.push(UserDetails(name, age));
+        kycDetails[name] = age;
     }
     
-    // function fetches all stored details in the array
-    function getDetail() public view  returns (UserDetails[] memory) {
-        return detailArr;
+    // function fetches the age of the person calling it, once name is entered.
+    function getDetail(string memory name) public view  returns (uint) {
+        return  kycDetails[name];
         
     }
 }   
